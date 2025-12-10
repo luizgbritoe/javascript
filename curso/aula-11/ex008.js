14c9368ce4e0ec2c5a28a9ad871f82c5f566f770
Luiz Brito <lguilherme.esperanca@gmail.com>
2025-12-10 12:16:57 -0300

    estudos de js

---
+++ curso/aula-11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+if (vel <=60){
+    console.log('Você está dentro da velocidade permitida na via.')
+} else {
+    console.log('Você ultrapassou o limite de velocidade da via. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança.')
